29488577848b60882d9ed76a4d31ff30a7728254
Tanvir Hossain <tanvirhossain.butex@gmail.com>
2024-12-28 22:57:36 +0600

    storing section in lowercase

--- backend/src/news-storage/services/news-storage.service.ts
+++ backend/src/news-storage/services/news-storage.service.ts
@@ -83,6 +83,7 @@ export class NewsStorageService {
 
         const article = new NewsArticleEntity({
             ...articleDto,
+            section: articleDto.section.toLowerCase(),
             author: user.name,
         });
 
