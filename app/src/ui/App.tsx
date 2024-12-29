04b4281b962b25aef9351ef3396413266afd0f0f
Tanvir Hossain <tanvirhossain.butex@gmail.com>
2024-12-29 14:37:18 +0600

    optimized frontend codebase

--- app/src/ui/App.tsx
+++ app/src/ui/App.tsx
@@ -1,31 +1,26 @@
 import { BrowserRouter, Routes, Route } from "react-router";
 import { Toaster } from "react-hot-toast";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import NewsList from "./custom-components/NewsList";
-import NewsDetails from "./custom-components/NewsDetails";
-import Bookmarks from "./custom-components/Bookmarks";
-import Auth from "./custom-components/Auth";
-
-const queryClient = new QueryClient();
+import NewsList from "./components/NewsList";
+import NewsDetails from "./components/NewsDetails";
+import Bookmarks from "./components/Bookmarks";
+import Auth from "./components/Auth";
 
 function App() {
   return (
     <>
-      <QueryClientProvider client={queryClient}>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<NewsList />} />
-            <Route
-              path="/section/:sectionName"
-              element={<NewsList isSection />}
-            />
-            <Route path="/news-details/:articleId" element={<NewsDetails />} />
-            <Route path="/bookmarks" element={<Bookmarks />} />
-            <Route path="/login" element={<Auth defaultScreen="login" />} />
-            <Route path="/signup" element={<Auth defaultScreen="signup" />} />
-          </Routes>
-        </BrowserRouter>
-      </QueryClientProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<NewsList />} />
+          <Route
+            path="/section/:sectionName"
+            element={<NewsList isSection />}
+          />
+          <Route path="/news-details/:articleId" element={<NewsDetails />} />
+          <Route path="/bookmarks" element={<Bookmarks />} />
+          <Route path="/login" element={<Auth defaultScreen="login" />} />
+          <Route path="/signup" element={<Auth defaultScreen="signup" />} />
+        </Routes>
+      </BrowserRouter>
       <Toaster />
     </>
   );
